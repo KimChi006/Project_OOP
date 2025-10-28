f69c29664da9e8af34076d313e8b1879dc06a69a
Vo Thi Thuy <thuyxinhgai0409@gmail.com>
2025-10-28 11:07:05 +0700

    Add files via upload

---
+++ Diem.java
@@ -0,0 +1,91 @@
+import model.MonHoc;
+import java.util.Scanner;
+abstract class Diem{
+    private HocSinh maHS;
+    private MonHoc maMon;
+    private double giaTri;
+public Diem(){}
+public Diem(HocSinh maHS,MonHoc monHoc, double giaTri){
+    this.maHS=maHS;
+    this.monHoc=monHoc;
+    this.giaTri=giaTri;
+}
+public void setmaHS(HocSinh maHS){
+    this.maHS=maHS;
+}
+public getmaHS(){
+    return maHS;
+}
+public void setmonHoc(MonHoc monHoc){
+    this.monHoc=monHoc;
+}
+public MonHoc getmonHoc(){
+    return monHoc;
+}
+public void setgiaTri(double giaTri){
+    this.giaTri=giaTri;
+}
+public double getgiaTri(){
+    return giaTri;
+}
+public abstract void nhap();
+public abstract void xuat();
+}
+class DiemMieng extends Diem{
+    public DiemMieng(double giaTri){
+         super(giaTri);
+    }
+    public void nhap(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("nhap diem mieng: ");
+         setgiaTri(sc.nextDouble());
+    }
+    public void xuat(){
+        System.out.println("diem mieng cua hocj sinh la: "+getgiaTri());
+    }
+}
+class Diem15Phut extends Diem {
+    public Diem15Phut(double giaTri) {
+        super(giaTri);
+    }
+
+    @Override
+    public void nhap(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("nhap diem 15p: ");
+         setgiaTri(sc.nextDouble());
+    }
+     public void xuat(){
+        System.out.println("diem 15p cua hoc sinh la: "+getgiaTri());
+    }
+
+}
+class Diem1Tiet extends Diem {
+    public Diem1Tiet(double giaTri) {
+        super(giaTri);
+    }
+
+    @Override
+    public void nhap(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("nhap diem 1 tiet: ");
+         setgiaTri(sc.nextDouble());
+    }
+         public void xuat(){
+        System.out.println("diem 1 tet cua hoc sinh la: "+getgiaTri());
+    }
+}
+class DiemThi extends Diem{
+    public DiemThi(double giaTri){
+        super(giaTri);
+    }
+    @Override
+    public void nhap(){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("nhap diem thi: ");
+         setgiaTri(sc.nextDouble());
+    }
+    public void xuat(){
+        System.out.println("diem thi cua hoc sinh la: "+getgiaTri());
+    }
+}
