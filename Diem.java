bac04b4a13c97ed97c05dcef72127af43b50e069
Vo Thi Thuy <thuyxinhgai0409@gmail.com>
2025-10-30 08:54:29 +0700

    Update Diem.java

--- Diem.java
+++ Diem.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Diem{
     private HocSinh maHS;
     private MonHoc maMon;
-    private double giaTri;
+    private double DiemMieng;
+    private double Diem15P;
+    private double Diem1T;
+    private double DiemThi;
 public Diem(){}
-public Diem(HocSinh maHS,MonHoc maMon, double giaTri){
+public Diem(HocSinh maHS,MonHoc maMon,double DiemMieng, double Diem15P, double Diem1T, double DiemThi){
     this.maHS=maHS;
     this.monHoc=maMon;
-    this.giaTri=giaTri;
+    this.DiemMieng=DiemMieng;
+    this.Diem15P=Diem15P;
+    this.Diem1T=Diem1T;
+    this.DiemThi=DiemThi;
 }
 public void setmaHS(HocSinh maHS){
     this.maHS=maHS;
@@ -22,72 +28,52 @@ public void setmonHoc(MonHoc maMon){
 public MonHoc getmaMon(){
     return maMon;
 }
-public void setgiaTri(double giaTri){
-    this.giaTri=giaTri;
+public void setDiemMieng(double DiemMieng){
+    this.DiemMieng=DiemMieng;
 }
-public double getgiaTri(){
-    return giaTri;
+public double getDiemMieng(){
+    return DiemMieng;
 }
-public abstract void nhap();
-public abstract void xuat();
+public void setDiem15P(double Diem15P){
+    this.Diem15P=Diem15P;
 }
-class DiemMieng extends Diem{
-    public DiemMieng(double giaTri){
-         super(giaTri);
-    }
-    public void nhap(){
-         Scanner sc=new Scanner(System.in);
-         System.out.println("nhap diem mieng: ");
-         setgiaTri(sc.nextDouble());
-    }
-    public void xuat(){
-        System.out.println("diem mieng cua hoc sinh la: "+getgiaTri());
-    }
+public double getDiem15P(){
+    return Diem15P;
 }
-class Diem15Phut extends Diem {
-    public Diem15Phut(double giaTri) {
-        super(giaTri);
-    }
-
-    @Override
-    public void nhap(){
-         Scanner sc=new Scanner(System.in);
-         System.out.println("nhap diem 15p: ");
-         setgiaTri(sc.nextDouble());
-    }
-     public void xuat(){
-        System.out.println("diem 15p cua hoc sinh la: "+getgiaTri());
-    }
-
+public void setDiem1T(double Diem1T){
+    this.Diem1T=Diem1T;
 }
-class Diem1Tiet extends Diem {
-    public Diem1Tiet(double giaTri) {
-        super(giaTri);
-    }
-
-    @Override
-    public void nhap(){
-         Scanner sc=new Scanner(System.in);
-         System.out.println("nhap diem 1 tiet: ");
-         setgiaTri(sc.nextDouble());
-    }
-         public void xuat(){
-        System.out.println("diem 1 tet cua hoc sinh la: "+getgiaTri());
-    }
+public double getDiem1T(){
+    return Diem1T;
 }
-class DiemThi extends Diem{
-    public DiemThi(double giaTri){
-        super(giaTri);
-    }
-    @Override
-    public void nhap(){
-         Scanner sc=new Scanner(System.in);
-         System.out.println("nhap diem thi: ");
-         setgiaTri(sc.nextDouble());
-    }
-    public void xuat(){
-        System.out.println("diem thi cua hoc sinh la: "+getgiaTri());
-    }
+public void setDiemThi(double DiemThi){
+    this.DiemThi=DiemThi;
 }
-
-
+public double getDiemThi(){
+    return DiemThi;
+}
+public void nhap(){
+    Scanner sc=Scanner(System.in);
+    System.out.println("nhap ma so hoc sinh: ");
+    setmaHS(sc.nextLine());
+    System.out.println("nhap ma mon hoc cho hoc sinh: ");
+    setmaMon(sc.nextLine());
+    System.out.println("nhap diem mieng cho hoc sinh: ");
+    setDiemMieng(sc.nextDouble());
+    System.out.println("nhap diem 15p cho hoc sinh: ");
+    setDiem15P(sc.nextDouble());
+    System.out.println("nhap diem 1 tiet cho hoc sinh: ");
+    setDiem1T(sc.nextDouble());
+    System.out.println("nhap diem thi cho hoc sinh: ");
+    setDiemThi(sc.nextDouble());
+    
+}
+public void xuat(){
+    System.out.println("ma so hoc sinh la: "+maHS);
+    System.out.println("ma mon hoc cua hoc sinh la: "+maMon);
+    System.out.println("diem mieng hoc sinh la: "+DiemMieng);
+    System.out.println("diem 15p hoc sinh la: "+Diem15P);
+    System.out.println("diem 1 tiet hoc sinh la: "+Diem1T);
+    System.out.println("diem thi hoc sinh la: "+DiemThi);
+}
+}
