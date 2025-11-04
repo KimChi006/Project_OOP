8c7947f447d8394ae2f4e1283666160359dd3c2f
Vo Thi Thuy <thuyxinhgai0409@gmail.com>
2025-11-04 10:11:50 +0700

    Update Nguoi.java thêm ràng buộc

--- Nguoi.java
+++ Nguoi.java
@@ -38,13 +38,31 @@ public abstract class Nguoi {
     }
 
     public void nhap() {
-        System.out.println("Nhập họ tên người: ");
+        System.out.println("Nhập họ tên: ");
         sethoTen(sc.nextLine());
-        System.out.println("Nhập giới tính người: ");
-        setgioiTinh(sc.nextLine());
-        System.out.println("Nhập năm sinh người: ");
-        setnamSinh(sc.nextInt());
+    String gioiTinh;
+     while(true){
+         System.out.print("Nhập giới tính: ");
+         gioiTinh=sc.nextLine().trim();//bỏ khoảng trắng phía trước trong hàm nhập để so sánh equal trong if
+         if(gioiTinh.equalsIgnoreCase("Nam") || gioiTinh.equalsIgnoreCase("Nữ")) break;
+         System.out.println("giới tính phải là nam hoặc nữ, vui lòng nhập lại!");
     }
-
-    public abstract void hienThi();
+        setgioiTinh(gioiTinh);
+   int namSinh;
+    while (true) {
+        System.out.print("Nhập năm sinh: ");
+        try {
+            namSinh = Integer.parseInt(sc.nextLine());
+            if (namSinh <= 2025) break;
+            else System.out.println("năm sinh phải nhỏ hơn 2025, vui lòng nhập lại!.");
+        } catch (NumberFormatException e) {
+            System.out.println(" Năm sinh phải là số nguyên. Vui lòng nhập lại.");
+        }
+    }
+        setnamSinh(namSinh);
 }
+
+    public void hienThi() {};
+}
+
+
