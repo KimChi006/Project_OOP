4bc890c8ddb987032e2a70ce07093ff18e26a5f1
Vo Thi Thuy <thuyxinhgai0409@gmail.com>
2025-11-06 21:23:10 +0700

    Update Diem.java RÀNG BUỘC ĐIỂM

--- Diem.java
+++ Diem.java
@@ -39,32 +39,69 @@ public class Diem {
     public double getDiemThi() { return DiemThi; }
 
     public void nhap() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Nhập mã số học sinh: ");
         setmaHS(sc.nextLine());
 
         System.out.print("Nhập mã môn học: ");
         setmaMon(sc.nextLine());
+        
+        Double DiemMieng;
+        while (true) {
+            System.out.print("Nhập điểm miệng: ");
+            try {
+                DiemMieng=sc.nextDouble();
+                 if (DiemMieng <=10.0 && DiemMieng>=0) break;
+                      else System.out.println("điểm phải lớn hơn hoặc bằng 0 và nhỏ hơn hoặc bằng 10, vui lòng nhập lại!");
+            } catch (NumberFormatException e) {
+               
+                    } 
+              }
+        setDiemMieng(DiemMieng);
 
-        System.out.print("Nhập điểm miệng: ");
-        setDiemMieng(sc.nextDouble());
+        Double Diem15P;
+        while (true) {
+            System.out.print("Nhập điểm 15 phút: ");
+            try {
+                Diem15P=sc.nextDouble();
+                 if (Diem15P <=10.0 && Diem15P>=0) break;
+                     else System.out.println("điểm phải lớn hơn hoặc bằng 0 và nhỏ hơn hoặc bằng 10, vui lòng nhập lại!");
+            } catch (NumberFormatException e) {
+               
+                   } 
+                }
+        setDiem15P(Diem15P);
 
-        System.out.print("Nhập điểm 15 phút: ");
-        setDiem15P(sc.nextDouble());
+        Double Diem1T;
+        while (true) {
+            System.out.print("Nhập điểm 1 tiết: ");
+            try {
+                Diem1T=sc.nextDouble();
+                 if (Diem1T <=10.0 && Diem1T>=0) break;
+                     else System.out.println("điểm phải lớn hơn hoặc bằng 0 và nhỏ hơn hoặc bằng 10, vui lòng nhập lại!");
+            } catch (NumberFormatException e) {
+               
+                   } 
+               }
+        setDiem1T(Diem1T);
 
-        System.out.print("Nhập điểm 1 tiết: ");
-        setDiem1T(sc.nextDouble());
-
-        System.out.print("Nhập điểm thi: ");
-        setDiemThi(sc.nextDouble());
+        Double DiemThi;
+        while (true) {
+            System.out.print("Nhập điểm thi: ");
+            try {
+                DiemThi=sc.nextDouble();
+                 if (DiemThi <=10.0 && DiemThi>=0) break;
+                     else System.out.println("điểm phải lớn hơn hoặc bằng 0 và nhỏ hơn hoặc bằng 10, vui lòng nhập lại!");
+            } catch (NumberFormatException e) {
+               
+                    } 
+               }
+        setDiemThi(DiemThi);
     }
 
-    public void xuat() {
-        System.out.println("\n--- Thông tin điểm ---");
-        System.out.println("Mã học sinh: " + maHS);
-        System.out.println("Mã môn học: " + maMon);
-        System.out.println("Điểm miệng: " + DiemMieng);
-        System.out.println("Điểm 15 phút: " + Diem15P);
-        System.out.println("Điểm 1 tiết: " + Diem1T);
-        System.out.println("Điểm thi: " + DiemThi);
-    }
+ public void xuat() {
+        System.out.println(maHS + " ; " + maMon + " ; " + DiemMieng + " ; " + Diem15P + " ; " + Diem1T + " ; " + DiemThi);
+     
 }
+}
+
